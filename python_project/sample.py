73603eb32478e1c050f68b62b81a23deea42d629
ogawayuto <ogaoga11271127@gmail.com>
2025-01-07 12:45:15 +0000

    fix: add docstring example

--- python_project/sample.py
+++ python_project/sample.py
@@ -1,5 +1,28 @@
-a = 1
-b = 2
+def add(a: int, b: int) -> int:
+    """Add two numbers together.
 
-c = a + b
-d = "asdf"
+    This function takes two numbers, a and b, and returns their sum.
+    This is sample function to demonstrate how to write a function
+
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        Sum of a and b
+        Typical usage example:
+        add(2, 3)
+
+    Raises:
+        TypeError: if a or b is not an integer
+
+
+    """
+    try:
+        return a + b
+    except TypeError as e:
+        msg = "a and b must be integers"
+        raise TypeError(msg) from e
+
+
+def _private_add(a: int, b: int) -> int:
+    return a + b
