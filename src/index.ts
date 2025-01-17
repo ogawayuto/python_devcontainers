ab610b416abb3d1202504cd9dba177779876c08f
ogawayuto <ogaoga11271127@gmail.com>
2025-01-17 08:14:51 +0000

    feat: typescriptの設定 Fixes #6

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 function addNumbers(a: number, b: number): number {
     return a + b;
 }
+
+export { addNumbers };
---
+++ src/sample.ts
@@ -0,0 +1,3 @@
+import { addNumbers } from "@/index";
+
+console.log(addNumbers(1, 2)); // 3
